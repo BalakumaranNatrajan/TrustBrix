8f0f1585379d14d8c41bb6df0ad377b73fd48bd4
Balakumaran <balakumaran.n@mitosistech.com>
2019-04-17 18:02:00 -0700

    Comments added

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const boom = require('express-boom');
-const mongoose = require('mongoose');
-var chalk = require('chalk')
-const { settings } = require('./src/config/config.settings');
-const protectedrouter = require('./src/Plugins');
+const chalk = require('chalk')
+
 const SWAGGER_DOCUMENT = YAML.load('./swagger.yml');
 const router = require('./src/Plugins/auth/router');
+const { settings } = require('./src/config/config.settings');
+const protectedrouter = require('./src/Plugins');
 const logger = require('./src/lib/winston');
 require('colors');
 
+
 app.use(boom());
 
+//Morgan configuration for loggers
 app.use(morgan(function (tokens, req, res) {
   return [
     chalk.yellowBright(tokens.method(req, res)),
@@ -29,17 +32,21 @@ app.use(morgan('combined', { "stream": logger.stream }));
 logger.info('Working dir:' + process.cwd())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Swagger file 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(SWAGGER_DOCUMENT));
+
+//Server creation
 const port = process.env.PORT || settings.PORT;
 app.listen(port, () => {
-  console.log("-> Server running on port".cyan, port);
+  console.log("Server running on port".cyan, port);
 })
 
-
+//MongoDb connnection configuration
 const mongoUrl = settings.mongoUrl;
 mongoose.connect(mongoUrl, { useNewUrlParser: true }, (err) => {
   if (err) throw err
-  console.log("-> Mongo db connected successfully".cyan);
+  console.log("Mongo db connected successfully".cyan);
 });
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
